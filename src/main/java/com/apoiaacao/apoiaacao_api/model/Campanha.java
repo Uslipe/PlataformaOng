f6aa3a7b6f7899ed7599f506ea04a9c386b9d1b6
Uslipe <felipe.pereira.nave@gmail.com>
2025-01-20 03:27:17 -0300

    feat: criação da entidade campanha

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/model/Campanha.java
@@ -0,0 +1,32 @@
+package com.apoiaacao.apoiaacao_api.model;
+
+import java.time.LocalDate;
+
+public abstract class Campanha {
+
+    //O atributo "id" destacado no diagrama de classe será gerenciado pelo banco de dados de forma autoincremental
+    private ONG ongFundadora;
+    //Novamente, listas serão admistradas pelo banco
+    private LocalDate dataLimite;
+    private boolean encerrada;
+
+    public ONG getOngFundadora() {
+        return ongFundadora;
+    }
+    public void setOngFundadora(ONG ongFundadora) {
+        this.ongFundadora = ongFundadora;
+    }
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+    public boolean isEncerrada() {
+        return encerrada;
+    }
+    public void setEncerrada(boolean encerrada) {
+        this.encerrada = encerrada;
+    }
+
+}
