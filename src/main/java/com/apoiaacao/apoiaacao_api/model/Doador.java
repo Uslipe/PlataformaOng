9a24adec196739c51bb3190a21d468c72db47821
Uslipe <felipe.pereira.nave@gmail.com>
2025-01-19 21:50:14 -0300

    feat: criar entidade doador

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/model/Doador.java
@@ -0,0 +1,30 @@
+package com.apoiaacao.apoiaacao_api.model;
+
+public class Doador extends Usuario{
+    private String cpf;
+    //O atributo no diagrama de classe "lista de doações" não será implementado usando arraylist, mas sim com consultas personalizadas do banco
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome(){
+        return super.getNome();
+    }
+
+    public void setNome(String nome){
+        super.setNome(nome);
+    }
+
+    public String getEmail(){
+        return super.getEmail();
+    }
+
+    public void setEmail(String email){
+        super.setNome(email);
+    }
+}
