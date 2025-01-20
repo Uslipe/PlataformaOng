dc7c6c0ef31c615056f1035036c85fd062f96647
Uslipe <felipe.pereira.nave@gmail.com>
2025-01-20 03:34:22 -0300

    feat: criação da entidade campanha financeira

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/model/CampanhaFinanceira.java
@@ -0,0 +1,48 @@
+package com.apoiaacao.apoiaacao_api.model;
+
+import java.time.LocalDate;
+
+public class CampanhaFinanceira extends Campanha {
+    private double valorObjetivo;
+    private double totalArrecadado;
+
+    public double getValorObjetivo() {
+        return valorObjetivo;
+    }
+
+    public void setValorObjetivo(double valorObjetivo) {
+        this.valorObjetivo = valorObjetivo;
+    }
+
+    public double getTotalArrecadado() {
+        return totalArrecadado;
+    }
+
+    public void setTotalArrecadado(double totalArrecadado) {
+        this.totalArrecadado = totalArrecadado;
+    }
+
+    public ONG getOngFundadora(){
+        return super.getOngFundadora();
+    }
+    
+    public void setOngFundadora(ONG ongFundadora){
+        super.setOngFundadora(ongFundadora);
+    }
+
+    public LocalDate getDataLimite(){
+        return super.getDataLimite();
+    }
+
+    public void setDataLimite(LocalDate dataLimite){
+        super.setDataLimite(dataLimite);
+    }
+
+    public boolean isEncerrada(){
+        return super.isEncerrada();
+    }
+
+    public void setEncerrada(boolean encerrada){
+        super.setEncerrada(encerrada);
+    }
+}
