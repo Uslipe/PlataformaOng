ce351c71235d3afa3821e81032e6e4a16405a491
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-22 20:57:40 -0300

    feat: repositório de campanha de itens

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaDeItens.java
@@ -0,0 +1,9 @@
+package com.apoiaacao.apoiaacao_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apoiaacao.apoiaacao_api.model.CampanhaDeItens;
+
+public interface Repositorio_CampanhaDeItens extends JpaRepository<CampanhaDeItens, Integer>{
+
+}
