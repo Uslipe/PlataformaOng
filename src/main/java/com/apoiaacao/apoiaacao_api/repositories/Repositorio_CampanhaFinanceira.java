eb9d92b32df94a6ef149cf0ffa0e2311fbae2a62
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-22 20:58:00 -0300

    feat: repositório de campanha financeira

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaFinanceira.java
@@ -0,0 +1,9 @@
+package com.apoiaacao.apoiaacao_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apoiaacao.apoiaacao_api.model.CampanhaFinanceira;
+
+public interface Repositorio_CampanhaFinanceira extends JpaRepository<CampanhaFinanceira, Integer>{
+
+}
