45fd0ffbe6e3eca50fc0b9085d85c72e809db2d7
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-22 20:55:44 -0300

    feat: repositório de doação financeira

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoFinanceira.java
@@ -0,0 +1,9 @@
+package com.apoiaacao.apoiaacao_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apoiaacao.apoiaacao_api.model.DoacaoFinanceira;
+
+public interface Repositorio_DoacaoFinanceira extends JpaRepository<DoacaoFinanceira, Integer>{
+
+}
