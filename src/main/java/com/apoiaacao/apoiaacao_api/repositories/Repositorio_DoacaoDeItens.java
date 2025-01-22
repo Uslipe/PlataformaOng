56cddf73a692c822e331ad389b72455438b7288f
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-22 20:56:15 -0300

    feat: repositório de doação de itens

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoDeItens.java
@@ -0,0 +1,9 @@
+package com.apoiaacao.apoiaacao_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apoiaacao.apoiaacao_api.model.DoacaoDeItens;
+
+public interface Repositorio_DoacaoDeItens extends JpaRepository<DoacaoDeItens, Integer>{
+
+}
