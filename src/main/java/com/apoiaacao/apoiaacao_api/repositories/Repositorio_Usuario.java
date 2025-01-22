00d9d6c34955b556cf09f8df9c2e2adde8f133f4
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-22 20:29:52 -0300

    feat: Criação do repositório do usuário

---
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_Usuario.java
@@ -0,0 +1,9 @@
+package com.apoiaacao.apoiaacao_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apoiaacao.apoiaacao_api.model.Usuario;
+
+public interface Repositorio_Usuario extends JpaRepository<Usuario, Integer>{
+
+}
