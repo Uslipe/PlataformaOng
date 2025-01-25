c058fee2e43fa4cd5849fd47dad09e2b14d7c5b0
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:50:16 -0300

    update: repositório de usuário

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_Usuario.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_Usuario.java
@@ -1,9 +1,17 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.apoiaacao.apoiaacao_api.model.Usuario;
 
 public interface Repositorio_Usuario extends JpaRepository<Usuario, Integer>{
 
+ @Query("SELECT u FROM Usuario u WHERE u.idTipoDeUsuario = :idTipoDeUsuario")
+    List<Usuario> findByIdTipoDeUsuario(@Param("idTipoDeUsuario") int idTipoDeUsuario);
+
+ 
 }
