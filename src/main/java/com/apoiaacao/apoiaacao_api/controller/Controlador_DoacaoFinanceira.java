1cc6e03adf3e3e144c83af40e48df001090eaab7
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:52:01 -0300

    update: controlador doação financeira

--- src/main/java/com/apoiaacao/apoiaacao_api/controller/Controlador_DoacaoFinanceira.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/controller/Controlador_DoacaoFinanceira.java
@@ -1,5 +1,6 @@
 package com.apoiaacao.apoiaacao_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,15 @@ public class Controlador_DoacaoFinanceira {
     @PostMapping("/salvarDoacaoFinanceira")
     public void salvarDoacaoFinanceira(@RequestBody DoacaoFinanceira doacaoFinanceira) {
         Repositorio_DoacaoFinanceira.save(doacaoFinanceira);
+    }
+
+    @GetMapping ("/listarDoacoesFinanceiras")
+    public Iterable<DoacaoFinanceira> listarDoacoesFinanceiras() {
+        return Repositorio_DoacaoFinanceira.findAll();
+    }
+
+    @GetMapping("/listarDoacoesFinanceirasPorUsuario")
+    public Iterable<DoacaoFinanceira> listarDoacoesFinanceirasPorUsuario(@RequestBody int idUsuario) {
+        return Repositorio_DoacaoFinanceira.findByIdUsuario(idUsuario);
     }
 }
