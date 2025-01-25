e3cfaa1855a57b72516e38422d30b2dc45ee709e
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-25 20:34:27 -0300

    fix: repositórios simplificados

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaFinanceira.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaFinanceira.java
@@ -3,14 +3,11 @@ package com.apoiaacao.apoiaacao_api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.apoiaacao.apoiaacao_api.model.CampanhaFinanceira;
 
 public interface Repositorio_CampanhaFinanceira extends JpaRepository<CampanhaFinanceira, Integer>{
 
-  @Query("SELECT c FROM CampanhaFinanceira c WHERE c.idOng = :idOng")
-    List<CampanhaFinanceira> findByIdOng(@Param("idOng") int idOng);
+  List<CampanhaFinanceira> findByIdOng(int idOng);
 
 }
--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoDeItens.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoDeItens.java
@@ -1,13 +1,11 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.apoiaacao.apoiaacao_api.model.DoacaoDeItens;
 
 public interface Repositorio_DoacaoDeItens extends JpaRepository<DoacaoDeItens, Integer>{
 
-  @Query("SELECT d FROM DoacaoDeItens d WHERE d.idUsuario = :idUsuario")
-    Iterable<DoacaoDeItens> findByIdUsuario(int idUsuario);
+  Iterable<DoacaoDeItens> findByIdUsuario(int idUsuario);
 
 }
