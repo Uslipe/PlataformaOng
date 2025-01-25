cc030fd308de0ef9ff8f268aad2bfd5136366c7b
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:54:06 -0300

    update: repositório campanha financeira

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaFinanceira.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaFinanceira.java
@@ -1,9 +1,16 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.apoiaacao.apoiaacao_api.model.CampanhaFinanceira;
 
 public interface Repositorio_CampanhaFinanceira extends JpaRepository<CampanhaFinanceira, Integer>{
+
+  @Query("SELECT c FROM CampanhaFinanceira c WHERE c.idOng = :idOng")
+    List<CampanhaFinanceira> findByIdOng(@Param("idOng") int idOng);
 
 }
