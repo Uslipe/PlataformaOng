4200af95105acb175aa50f20ba32ccc6b7706b27
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:52:27 -0300

    update: repositório campanha de itens

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaDeItens.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_CampanhaDeItens.java
@@ -1,9 +1,16 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.apoiaacao.apoiaacao_api.model.CampanhaDeItens;
 
 public interface Repositorio_CampanhaDeItens extends JpaRepository<CampanhaDeItens, Integer>{
+
+  @Query("SELECT c FROM CampanhaDeItens c WHERE c.idOng = :idOng")
+    List<CampanhaDeItens> findByIdOng(@Param("idOng") int idOng);
 
 }
