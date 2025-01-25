cac62e7ade4dba9b0269438bcc7448c067cfbd12
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:51:35 -0300

    update: repositório doação financeira

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoFinanceira.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoFinanceira.java
@@ -1,9 +1,13 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.apoiaacao.apoiaacao_api.model.DoacaoFinanceira;
 
 public interface Repositorio_DoacaoFinanceira extends JpaRepository<DoacaoFinanceira, Integer>{
+
+  @Query("SELECT d FROM DoacaoFinanceira d WHERE d.idUsuario = :idUsuario")
+    Iterable<DoacaoFinanceira> findByIdUsuario(int idUsuario);
 
 }
