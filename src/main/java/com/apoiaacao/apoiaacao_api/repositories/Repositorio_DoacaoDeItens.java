95abf4e117cd56bf4c2000c2550957c82c141c4f
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2025-01-24 22:55:02 -0300

    update: repositório doação de itens

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoDeItens.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_DoacaoDeItens.java
@@ -1,9 +1,13 @@
 package com.apoiaacao.apoiaacao_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.apoiaacao.apoiaacao_api.model.DoacaoDeItens;
 
 public interface Repositorio_DoacaoDeItens extends JpaRepository<DoacaoDeItens, Integer>{
+
+  @Query("SELECT d FROM DoacaoDeItens d WHERE d.idUsuario = :idUsuario")
+    Iterable<DoacaoDeItens> findByIdUsuario(int idUsuario);
 
 }
