f9564ee9fc4f07935f1da4e2007cbeaa45d207a0
Felipe Reis <107627690+Uslipe@users.noreply.github.com>
2025-01-26 17:26:35 -0300

    Update Repositorio_Usuario.java

--- src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_Usuario.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/repositories/Repositorio_Usuario.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.apoiaacao.apoiaacao_api.model.Usuario;
 
 public interface Repositorio_Usuario extends JpaRepository<Usuario, Integer>{
-
+  Usuario findByEmail(String email);
   List<Usuario> findByIdTipoDeUsuario(int idTipoDeUsuario);
 
  
