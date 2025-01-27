f9162cb16d0d7a78e905cfeccb6ebff9273588ee
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-01-27 18:10:13 -0300

    feat: permissão de todas as requisiões http

--- src/main/java/com/apoiaacao/apoiaacao_api/config/SecurityConfig.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/config/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
         return http
                     .csrf(customizer -> customizer.disable()) //Desativamos o csrf pois isso é uma api, e os clientes enviam tokens de autenticação (JWT)
                     .authorizeHttpRequests(request -> request
-                                                            .requestMatchers("/salvarUsuario", "/listarCampanhasFinanceiras").permitAll()
-                                                            .anyRequest().authenticated()) //Todas as requisições de HTTP precisam ser autenticadas, no nosso caso, com httpBasic
+                                                            .requestMatchers("/salvarUsuario", "/listarCampanhasFinanceiras", "/*").permitAll()
+                                                            ) //Todas as requisições de HTTP precisam ser autenticadas, no nosso caso, com httpBasic
                     .httpBasic(Customizer.withDefaults()) //Aplica as configurações mais básicas de autenticação (no nosso caso, presente no application.properties)
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Define as sessões como stateless, comuns em API pelo trabalho de token
                     .build();
