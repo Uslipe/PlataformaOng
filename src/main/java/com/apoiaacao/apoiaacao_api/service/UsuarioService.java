66a6463edf90dde1cddbc3a15a6eb185cd1e4a80
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-02-13 13:35:25 -0300

    feat: adição de método para juntar as listas de doações do usuário

--- src/main/java/com/apoiaacao/apoiaacao_api/service/UsuarioService.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/service/UsuarioService.java
@@ -1,11 +1,17 @@
 package com.apoiaacao.apoiaacao_api.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.apoiaacao.apoiaacao_api.model.DoacaoDeItens;
+import com.apoiaacao.apoiaacao_api.model.DoacaoFinanceira;
+import com.apoiaacao.apoiaacao_api.model.DoacaoWrapper;
 import com.apoiaacao.apoiaacao_api.model.Usuario;
 import com.apoiaacao.apoiaacao_api.repositories.Repositorio_Usuario;
 
@@ -29,4 +35,28 @@ public class UsuarioService {
 
         return "Falha";
     }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return repositorioUsuario.findByEmail(email);
+    }
+
+    public List<DoacaoWrapper> juntarDoacoes(List<DoacaoFinanceira> doacoesFinanceiras, List<DoacaoDeItens> doacoesDeItens) {
+        List<DoacaoWrapper> todasDoacoes = new ArrayList<>();
+
+        // Adiciona todas as doações financeiras na lista de todas as doações
+        if (doacoesFinanceiras != null) {
+            for (DoacaoFinanceira doacaoFinanceira : doacoesFinanceiras) {
+                todasDoacoes.add(new DoacaoWrapper(doacaoFinanceira));
+            }
+        }
+        // Adiciona todas as doações de itens na lista de todas as doações
+        if (doacoesDeItens != null) {
+            for (DoacaoDeItens doacaoDeItens : doacoesDeItens) {
+                todasDoacoes.add(new DoacaoWrapper(doacaoDeItens));
+            }
+        }
+
+        return todasDoacoes;
+    }
+
 }
