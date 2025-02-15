48f008b84edaa2f1515df896fa8cdf732c11d8ac
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-02-15 14:39:07 -0300

    feat: listar campanhas não precisa de autorização

--- src/main/java/com/apoiaacao/apoiaacao_api/config/SecurityConfig.java
+++ src/main/java/com/apoiaacao/apoiaacao_api/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         return http
                     .csrf(customizer -> customizer.disable()) //Desativamos o csrf pois isso é uma api, e os clientes enviam tokens de autenticação (JWT)
                     .authorizeHttpRequests(request -> request
-                                                            .requestMatchers("/salvarUsuario", "/login").permitAll().anyRequest().authenticated())//Todas as requisições de HTTP precisam ser autenticadas, no nosso caso, com httpBasic
+                                                            .requestMatchers("/salvarUsuario", "/login", "/listarCampanhasFinanceiras", "/listarCampanhasDeItens").permitAll().anyRequest().authenticated())//Todas as requisições de HTTP precisam ser autenticadas, no nosso caso, com httpBasic
                     .httpBasic(Customizer.withDefaults()) //Aplica as configurações mais básicas de autenticação (no nosso caso, presente no application.properties)
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Define as sessões como stateless, comuns em API pelo trabalho de token
                     .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) //Criação de un novo filtro que antecede a validação de login e senha (filtro de jwt token)
