2f941c61cd9053b891b3289ca5ccf36c0da66faf
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-03-01 20:08:41 -0300

    feat: adição de logs pra melhor visualização dos testes

--- src/test/java/com/apoiaacao/apoiaacao_api/ControladorUsuarioTest2.java
+++ src/test/java/com/apoiaacao/apoiaacao_api/ControladorUsuarioTest2.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +26,7 @@ import com.apoiaacao.apoiaacao_api.repositories.Repositorio_Usuario;
 import com.apoiaacao.apoiaacao_api.service.EmailService;
 import com.apoiaacao.apoiaacao_api.service.UsuarioService;
 
+// Testes dos métodos relacionados ao Gerenciamento de Perfil dos Doadores
 @ExtendWith(MockitoExtension.class)
 public class ControladorUsuarioTest2 {
 
@@ -59,10 +62,12 @@ public class ControladorUsuarioTest2 {
 
     @Test
     public void testBuscarUsuario() {
-        System.out.println("Executando testBuscarUsuario");
+        System.out.println("\n-------- Executando testBuscarUsuario --------");
         when(repositorioUsuario.findById(anyInt())).thenReturn(Optional.of(usuario));
 
+        System.out.println("Procurando usuario com id = 1");
         Usuario resultado = controladorUsuario.buscarUsuario(1).getBody();
+        System.out.println("Usuario encontrado: " + resultado);
 
         assertNotNull(resultado);
         assertEquals(usuario.getId(), resultado.getId());
@@ -72,15 +77,17 @@ public class ControladorUsuarioTest2 {
 
     @Test
     public void testEditarPerfil() {
-        System.out.println("Executando testEditarPerfil");
+        System.out.println("\n-------- Executando testEditarPerfil --------");
         when(repositorioUsuario.findById(anyInt())).thenReturn(Optional.of(usuario));
         when(repositorioUsuario.save(any(Usuario.class))).thenReturn(usuario);
 
+        System.out.println("Antes da atualizacao dos dados: " + usuario);
         Usuario usuarioAtualizado = new Usuario();
         usuarioAtualizado.setNome("Usuario Atualizado");
         usuarioAtualizado.setEmail("atualizado@example.com");
 
         Usuario resultado = controladorUsuario.editarPerfil(1, usuarioAtualizado).getBody();
+        System.out.println("Depois da atualizacao dos dados: " + resultado);
 
         assertNotNull(resultado);
         assertEquals(usuarioAtualizado.getNome(), resultado.getNome());
@@ -89,13 +96,14 @@ public class ControladorUsuarioTest2 {
 
     @Test
     public void testDeletarUsuario() {
-        System.out.println("Executando testDeletarUsuario");
+        System.out.println("\n-------- Executando testDeletarUsuario --------");
         when(repositorioUsuario.findById(anyInt())).thenReturn(Optional.of(usuario));
 
         Usuario resultado = controladorUsuario.deletarUsuario(1).getBody();
 
         assertNotNull(resultado);
         assertEquals(usuario.getId(), resultado.getId());
+        System.out.println("Deletando usuario com id = " + usuario.getId());
         verify(repositorioUsuario).delete(usuario);
     }
 
